c3fa0bdf96b2b2d8a1326c53633e9ab5316583b6
gouveazs <joaovitorgouveaanasthacio12@gmail.com>
2026-03-03 21:15:37 -0300

    Login permanente - ao fechar o app e abrir novamente não é necessário logar de novo

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -12,7 +12,7 @@ router.post('/login', authController.login);
 router.get('/google/config', authGoogleController.getGoogleConfig);
 router.post('/google/login', authGoogleController.googleLogin);
 router.post('/google/register', authGoogleController.googleRegister);
-//router.post('/refresh', authController.handleRefresh);
+router.post('/refresh', authController.handleRefresh);
 
 
 module.exports = router;
